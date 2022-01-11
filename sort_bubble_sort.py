a3087aa287a05a3e493ad4b5d0b922ccc0b4fe2a
Pavan Aditya <84063768+pavanadityamaganti@users.noreply.github.com>
2022-01-11 16:42:49 +0530

    Update sort_bubble_sort.py

--- sort_bubble_sort.py
+++ sort_bubble_sort.py
@@ -6,7 +6,5 @@ for i in range(0,n):
 for i in range(len(l)):
     for j in range(len(l)-1):
         if l[j]>l[j+1]:
-            temp=l[j]
-            l[j]=l[j+1]
-            l[j+1]=temp
+            l[j],l[j+1]=l[j+1],l[j]
 print(l)
